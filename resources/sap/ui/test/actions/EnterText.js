c93351edc87a4031b5891c97d8ef97171ae0fca2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-09 09:31:11 +0000

    Deploy SAP/ui5-flush-game to github.com/SAP/ui5-flush-game.git:gh-pages

--- resources/sap/ui/test/actions/EnterText.js
+++
@@ -1,6 +0,0 @@
-/*!
- * OpenUI5
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
- * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
- */
-sap.ui.define(["sap/ui/test/actions/Action","sap/ui/events/KeyCodes"],function(t,e){"use strict";return t.extend("sap.ui.test.actions.EnterText",{metadata:{properties:{text:{type:"string"},clearTextFirst:{type:"boolean",defaultValue:true}},publicMethods:["executeOn"]},executeOn:function(t){var i=this.$(t),r=i[0];if(!r){return}if(this.getText()===undefined||!this.getClearTextFirst()&&!this.getText()){this.oLogger.error("Please provide a text for this EnterText action");return}var s=this.getUtils();this.oLogger.timestamp("opa.actions.enterText");this.oLogger.debug("Enter text in control "+t);this._tryOrSimulateFocusin(i,t);if(this.getClearTextFirst()){s.triggerKeydown(r,e.DELETE);s.triggerKeyup(r,e.DELETE);i.val("");s.triggerEvent("input",r)}var n=i.val();this.getText().split("").forEach(function(t){n+=t;s.triggerCharacterInput(r,t,n);s.triggerEvent("input",r)});this._simulateFocusout(r);s.triggerEvent("search",r)}})});
