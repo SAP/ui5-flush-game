b771d34a56ac89e0827ef040f53242371bab8fd2
Michael Graf <mi.graf@sap.com>
2019-05-08 20:33:35 +0200

    outsource loading script to separate file to load earlier

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,34 +1,5 @@
 /* global createjs */
 
-(function () {
-	/* update loading status with amazing messages */
-	var aStates = [
-		"Checking Faulty Circuits",
-		"Charging Chromatic Laser",
-		"Rebooting AI Core System",
-		"Instantiating Evil Robot Army",
-		"Polishing communication antenna",
-		"Booting up sound system",
-		"Inspecting Software Bugs",
-		"Analyzing Data Lake",
-		"Taming Render Manager",
-		"Updating Fusion Drive"
-	];
-
-	function updateStatus () {
-		setTimeout(function () {
-			var iRandom = Math.floor(Math.random() * aStates.length);
-			document.getElementsByClassName("status")[0].innerHTML = aStates[iRandom];
-			aStates.splice(iRandom, 1);
-			if (aStates.length > 0) {
-				updateStatus();
-			}
-		}, 300 + Math.random() * 500);
-	}
-
-	updateStatus();
-})();
-
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
