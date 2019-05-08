790291f44b83c1216aaf8b979dc0db0d33950a5f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-08 18:35:47 +0000

    Deploy SAP/ui5-flush-game to github.com/SAP/ui5-flush-game.git:gh-pages

---
+++ loading-dbg.js
@@ -0,0 +1,28 @@
+/* update loading screen with amazingly funny status messages */
+(function () {
+	var aStates = [
+		"Checking Faulty Circuits",
+		"Charging Chromatic Laser",
+		"Rebooting AI Core System",
+		"Instantiating Evil Robot Army",
+		"Polishing communication antenna",
+		"Booting up sound system",
+		"Inspecting Software Bugs",
+		"Analyzing Data Lake",
+		"Taming Render Manager",
+		"Updating Fusion Drive"
+	];
+
+	function updateStatus () {
+		setTimeout(function () {
+			var iRandom = Math.floor(Math.random() * aStates.length);
+			document.getElementsByClassName("status")[0].innerHTML = aStates[iRandom];
+			aStates.splice(iRandom, 1);
+			if (aStates.length > 0) {
+				updateStatus();
+			}
+		}, 300 + Math.random() * 500);
+	}
+
+	updateStatus();
+})();
