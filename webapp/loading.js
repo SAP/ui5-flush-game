089536803762103332a7858310a483b959c8f79f
Michael Graf <mi.graf@sap.com>
2019-05-09 10:59:50 +0200

    harmonize loading messages

--- webapp/loading.js
+++ webapp/loading.js
@@ -1,22 +1,22 @@
 /* update loading screen with amazingly funny status messages */
 (function () {
 	var aStates = [
-		"Checking Faulty Circuits",
-		"Charging Chromatic Laser",
-		"Rebooting AI Core System",
-		"Instantiating Evil Robot Army",
-		"Polishing communication antenna",
+		"Checking faulty circuits",
+		"Charging chromatic laser",
+		"Rebooting AI core system",
+		"Instantiating evil robot army",
+		"Polishing communication antennas",
 		"Booting up sound system",
-		"Inspecting Software Bugs",
-		"Analyzing Data Lake",
-		"Taming Render Manager",
-		"Updating Fusion Drive"
+		"Inspecting software bugs",
+		"Analyzing data lakes",
+		"Taming render manager",
+		"Updating fusion drive"
 	];
 
 	function updateStatus () {
 		setTimeout(function () {
 			var iRandom = Math.floor(Math.random() * aStates.length);
-			document.getElementsByClassName("status")[0].innerHTML = aStates[iRandom];
+			document.getElementsByClassName("status")[0].innerHTML = aStates[iRandom] + "...";
 			aStates.splice(iRandom, 1);
 			if (aStates.length > 0) {
 				updateStatus();
